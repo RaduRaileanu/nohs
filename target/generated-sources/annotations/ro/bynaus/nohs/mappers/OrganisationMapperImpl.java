6a5509b88d6f4f9199c5c527f404b2efce6fd06c
RaduRaileanu <67751805+RaduRaileanu@users.noreply.github.com>
2024-01-31 09:50:16 +0200

    gitignore update

--- target/generated-sources/annotations/ro/bynaus/nohs/mappers/OrganisationMapperImpl.java
+++ target/generated-sources/annotations/ro/bynaus/nohs/mappers/OrganisationMapperImpl.java
@@ -13,7 +13,7 @@ import ro.bynaus.nohs.models.SubscriptionDTO;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-28T11:29:59+0200",
+    date = "2024-01-29T16:38:14+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21 (Oracle Corporation)"
 )
 @Component
